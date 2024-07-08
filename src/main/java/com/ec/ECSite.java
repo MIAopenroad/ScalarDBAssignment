3041f88def9f75469cbfbf6a30bf334e0f702a91
Taichi Araki <araki130710@gmail.com>
2024-07-08 12:21:27 +0900

    Success to migrate into spring-boot appliaction.

---
+++ src/main/java/com/ec/ECSite.java
@@ -0,0 +1,11 @@
+package com.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECSite {
+    public static void main(String[] args) {
+        SpringApplication.run(ECSite.class, args);
+    }
+}
